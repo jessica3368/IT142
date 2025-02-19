2472441c9dce797e0040d2962e33ce3258a1f202
unknown <jessica.santos13235@gmail.com>
2025-02-19 22:54:59 +0800

    Updated hero.css and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import Hero from './components/hero'
-import './App.css'
-function App(){
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Hero from "./components/hero";
+import About from "./components/about";
+import "./style/hero.css"
+import "../src/App";
 
+function App() {
   return (
-    <>
-     <Hero/>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
