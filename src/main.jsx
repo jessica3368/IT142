412db6efb9d2ba18202f34cf244a42e6fb27f57e
unknown <jessica.santos13235@gmail.com>
2025-02-19 22:13:18 +0800

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App"; // Ensure this path is correct
+import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
